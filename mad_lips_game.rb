266c6519045966b16d0c4bc58992b53e626046cc
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2020-08-20 19:24:41 +0300

    Add files via upload

---
+++ mad_lips_game.rb
@@ -0,0 +1,58 @@
+print "İsminizi girin: "
+isim = gets.chomp()
+puts "\n"
+print "Yaşınızı girin: "
+age = gets.chomp()
+puts "\n"
+print "Bir meslek girin(Trolleyebilirsiniz. Örneğin ejderha avcılığı.): "
+job = gets.chomp()
+puts "\n"
+print "Bir yıl girin: "
+yıl = gets.chomp()
+puts "\n"
+print "Troll bir isim girin: "
+friend_name1 = gets.chomp()
+puts "\n"
+print "En sevdiğiniz yemeği girin: "
+yemek = gets.chomp()
+puts "\n"
+print "Kötüler grubu oluştursanız ismi ne olurdu: "
+bad_team_name = gets.chomp()
+puts "\n"
+print "Silah ismi girin: "
+silah1 = gets.chomp()
+puts "\n"
+print "Bir renk girin: "
+renk = gets.chomp()
+puts "\n"
+print "Başka bir renk girin: "
+renk2 = gets.chomp()
+puts "\n"
+print "Bir renk daha girin: "
+renk3 = gets.chomp()
+puts "\n"
+print "Tamam tamam bu son. Son bir renk girin: "
+renk4 = gets.chomp()
+puts "\n"
+print "En sevdiğiniz meyveyi girin: "
+meyve = gets.chomp()
+puts "\n"
+print "Bir eylem girin (Ağlıyor, kaçıyor, uçuyor, vuruyor gibi.): "
+eylem = gets.chomp()
+puts "\n"
+print "Bir meyve girin: "
+meyve2 = gets.chomp()
+
+puts "\n\n"
+
+puts "Merhaba! Bu senin boşlukları doldurarak oluşturduğun bir hikaye.
+Bu hikayede senin adın #{isim}. #{age} yaşındasın ve #{job} işinde çalışıyorsun.
+Yıl #{yıl}. Büyük ve zengin bir mekandasın. İçinden 'resmen kale gibi!' diye düşünüyorsun. Arkadaşın #{friend_name1} ile #{yemek} yiyorsunuz.
+O mekanın gerçek bir savaş kalesi olduğu aklına bile gelmiyor. #{yıl.to_i - 5} yılında yeni bir teknoloji devrimi başlamış ve #{bad_team_name} adlı bir grup bu teknoloji devrimini kötüye kullanıyor.
+İnsanları #{silah1} kullanarak kitleleri yönetebiliyor ve sen ve arkadaşın bunu bilmeden #{yemek} yiyorsunuz. 
+Tam karşında #{renk} renkli kocaman bir kapı var. Kapıyı dikkatlice inceliyorsun. Tam sen incelerken kapı aniden #{renk2} , aşırı parlak bir ışık saçmaya başlıyor.
+Kapı yavaş yavaş açıldıkça kapının arkasında duran #{renk3} güneş gözlüklü, #{renk4} parlak ceketli, elinde #{meyve} silahı olan bir kişi görüyorsun.
+Etrafındaki insan topluluğu olabildiğince hızlı #{eylem} ve mekanı terk ediyor. Sen daha ne olduğunu anlayamadan arkadaşın aniden yemeği bırakıp ayağa kalkıyor. 
+Arkadaşının elinde gördüğün #{meyve2} silahını nereden ve ne zaman çıkarttı hiçbir fikrin yok. Oturmuş olanları izliyorsun. Ve birden alarmın çalıyor. Uyanıyorsun.
+
+"
